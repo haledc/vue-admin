68b8de7b008413c579553131859d0a7458c67710
Hale Deng <haledeng88@gmail.com>
2019-03-05 10:38:15 +0800

    update config

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
     devServer: {
       proxy: {
         '/api/*': {
-          target: 'http://127.0.0.1:3000'
+          target: 'http://127.0.0.1:9098'
         }
       }
     }
